a01e397a4131f341b20244b083087b9cbbada79d
harshitap2013 <hari2013.pr@gmail.com>
2020-07-20 21:45:18 +0530

    tets

---
+++ src/main/java/seleniumTest/Selenium_test.java
@@ -0,0 +1,36 @@
+package seleniumTest;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Selenium_test {
+	
+	public static Properties prop;
+	public static WebDriver driver;
+	public static WebDriverWait wait;
+	
+	@Test
+	@Parameters(value= {"Browser"})
+	public static void initialise(String Browser) {
+		
+		if(Browser.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--incognito");
+			options.addArguments("---disable-notifications" );
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver(options);
+			
+			driver.navigate().to("https://www.google.com");
+			}
+
+}
+	
+}
